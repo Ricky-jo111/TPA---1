28cff6896df61b4f833afa10d0f1a58fa42cb658
Ricky Jonathan <rickyjonathan60@gmail.com>
2022-10-09 12:14:50 +0700

    first commit TPA-1

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-input", {
+    strings : ["Web Development", "Front-End Web Development"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    loop : true
+  })
+
+  AOS.init();
